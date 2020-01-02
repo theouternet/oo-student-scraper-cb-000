79d2dca75b72187f1ff0d060d6fadc99c5045b09
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2020-01-02 22:50:01 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,4 +1,4 @@
-
+require scraper.rb
 
 
 class Student
@@ -14,7 +14,9 @@ class Student
   end
 
   def self.create_from_collection(students_array)
-    
+    students_array.each do |student|
+      Student.new(student)
+    end
   end
 
   def add_student_attributes(attributes_hash)
